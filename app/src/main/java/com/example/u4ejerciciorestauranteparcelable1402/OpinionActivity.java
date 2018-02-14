5ea815d8a547255cfb3d030f2edab3482138f914
Javi <orte.mkt@gmail.com>
2018-02-14 13:28:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/u4ejerciciorestauranteparcelable1402/OpinionActivity.java
@@ -0,0 +1,13 @@
+package com.example.u4ejerciciorestauranteparcelable1402;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpinionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opinion);
+    }
+}
